bb2ece16d5c677522d570171cec51182334d42a5
박석채 <qkrtjrco130@gmail.com>
2020-04-21 12:11:44 +0900

    어플 초기형태 업로드

---
+++ app/src/main/java/com/example/a3thproject/mJoinActivity.java
@@ -0,0 +1,14 @@
+package com.example.a3thproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mJoinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_m_join);
+    }
+}
