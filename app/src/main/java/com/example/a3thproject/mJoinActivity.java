7d8e80eb34785c14a473f750a0b8e8e21e59ed59
박석채 <qkrtjrco130@gmail.com>
2020-04-22 14:52:05 +0900

    타이틀, 상태바 감추기 => 타이틀 주석 코드는 액티브마다 반드시 추가하고 임포트하기

--- app/src/main/java/com/example/a3thproject/mJoinActivity.java
+++ app/src/main/java/com/example/a3thproject/mJoinActivity.java
@@ -3,12 +3,18 @@ package com.example.a3thproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class mJoinActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // 타이틀 메뉴(상단) 감추기
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_m_join);
     }
 }
