b433d97e258d07496815c93a00972aa3b61059a9
박석채 <qkrtjrco130@gmail.com>
2020-04-24 14:16:39 +0900

    카카오 API 로그인 기능 text

--- app/src/main/java/com/example/a3thproject/LoginActivity.java
+++ app/src/main/java/com/example/a3thproject/LoginActivity.java
@@ -1,13 +1,18 @@
 package com.example.a3thproject;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.AuthFailureError;
@@ -21,26 +26,28 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.security.MessageDigest;
 import java.util.HashMap;
 import java.util.Map;
 
-public class jjjj extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
-    EditText text;
-    EditText pw;
+    EditText id,pw;
     Button join;
     static RequestQueue requestQueue;
-   StringRequest request;
-   String id;
-
+    StringRequest request;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jjjj);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
 
-        text = findViewById(R.id.text);
+        getAppKeyHash();
+
+        id = findViewById(R.id.text);
         pw = findViewById(R.id.pw);
         join = findViewById(R.id.join);
         if(requestQueue == null){
@@ -50,72 +57,69 @@ public class jjjj extends AppCompatActivity {
         join.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 testJson();
-
             }
         });
-
-
     }
 
     public void testJson(){
         String url = "http://192.168.56.1:8081/Podo/Loginserice";
-
-
         request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
                 try {
                     JSONObject jObj = new JSONObject(response);
                     boolean error = jObj.getBoolean("error");
-
                     if (!error) {
-
-
                     } else {
-
-                        Toast.makeText(jjjj.this, "요청에 실패했습니다 : 서버 오류", Toast.LENGTH_SHORT).show();
-
+                        Toast.makeText(LoginActivity.this, "요청에 실패했습니다 : 서버 오류", Toast.LENGTH_SHORT).show();
                     }
-
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
-
                 println(response);
-
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-
             }
-
         }){
             @Override
             protected Map<String, String> getParams() throws AuthFailureError {
                 Map<String, String> params = new HashMap<String, String>();
-                params.put("id", text.getText().toString());
+                params.put("id", id.getText().toString());
                 params.put("pw", pw.getText().toString());
                 return params;
             }
         };
-
         request.setShouldCache(false);
         requestQueue.add(request);
     }
 
     public void println(String data){
-
         if (data.equals("true")){
             Log.v("son",data);
-
-            Intent intent = new Intent(jjjj.this, MenuActivity.class);
+            Intent intent = new Intent(LoginActivity.this, MenuActivity.class);
             startActivity(intent);
         }else{
-            Toast.makeText(jjjj.this, "로그인 실패 다시 확인해주세요.", Toast.LENGTH_SHORT).show();
+            Toast.makeText(LoginActivity.this, "로그인 실패 다시 확인해주세요.", Toast.LENGTH_SHORT).show();
         }
-
+    }
+    // 해시 키 수집 코드
+    private void getAppKeyHash() {
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(),
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md;
+                md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                String something = new String(Base64.encode(md.digest(), 0));
+                Log.e("Hash key", something);
+            }
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            Log.e("name not found", e.toString());
+        }
     }
 }
