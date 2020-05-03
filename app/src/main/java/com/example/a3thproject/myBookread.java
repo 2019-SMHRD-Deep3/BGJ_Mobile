c2767d313270175a7c3bd3a98a2522b12a0014b5
지성은 <jsunge0907@gmail.com>
2020-05-03 14:49:10 +0900

    책읽기 페이지 전환

--- app/src/main/java/com/example/a3thproject/myBookread.java
+++ app/src/main/java/com/example/a3thproject/myBookread.java
@@ -6,37 +6,40 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
-import android.widget.QuickContactBadge;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
 
 public class myBookread extends AppCompatActivity {
     WebView webView;
     Button btn1, btn2;
+    int num = 0;
 
     @SuppressLint("SourceLockedOrientationActivity")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_bookread);
-
-
-
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);   // 화면 세로고정 코드
-                                                                             //     가로고정 코드 -> SCREEN_ORIENTATION_LANDSCAPE
+        //     가로고정 코드 -> SCREEN_ORIENTATION_LANDSCAPE
 
 
         webView = (WebView) findViewById(R.id.webvw);
         WebSettings webSettings = webView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
+        webSettings.setJavaScriptEnabled(true);   // 웹뷰 자바스크립트 true
 
         // 웹뷰 멀티 터치 가능하게 (줌기능)
         webSettings.setBuiltInZoomControls(true);   // 줌 아이콘 사용
@@ -51,12 +54,39 @@ public class myBookread extends AppCompatActivity {
             }
         });
 
-        webView.loadUrl("http://172.30.1.17:8081/Podo/Book.jsp");
+        webView.loadUrl("http://172.30.1.17:8081/Podo/Book.jsp?num="+num);
 
+        btn1.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    Toast.makeText(getApplicationContext(), "왼쪽 클릭", Toast.LENGTH_LONG).show();
+                    num -= 1;
+                    if(num<0) {
+                        num = 0;
+                    }
+                    webView.loadUrl("http://172.30.1.17:8081/Podo/Book.jsp?num=" + num);
 
+                } return false;
+            }
+        });
 
+        btn2.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    Toast.makeText(getApplicationContext(), "오른쪽 클릭", Toast.LENGTH_LONG).show();
+                    num += 1;
+                    webView.loadUrl("http://172.30.1.17:8081/Podo/Book.jsp?num=" + num);
 
+                }
+                return false;
+            }
+
+        });
 
 
     }
+
+
 }
