6d41cbf983a69d0ce8f692e793d3a7d6a6f4c2f7
박석채 <qkrtjrco130@gmail.com>
2020-05-08 12:29:06 +0900

    풀 받음1

--- app/src/main/java/com/example/a3thproject/MenuActivity.java
+++ app/src/main/java/com/example/a3thproject/MenuActivity.java
@@ -17,6 +17,22 @@ import android.widget.Toolbar;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MenuActivity extends AppCompatActivity {
 
     ImageView menu1, menu2, menu3;
@@ -24,6 +40,7 @@ public class MenuActivity extends AppCompatActivity {
     Button Pop;
     Intent intent;
     static RequestQueue requestQueue;
+    MenuInflater inflater;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
